6017cefb00bf5752bc1524e17259111f6e6b2a36
Gerikawa4545 <kengobaseball4@gmail.com>
2021-01-12 16:36:31 +0900

    1st commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9b29dac10eedb71fbb6ff947abc7c84997d41f8d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9b29dac10eedb71fbb6ff947abc7c84997d41f8d',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.5.0',
+      'version' => '4.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7a6da5e3e7ad7c749dde806546a35d4d4259d965',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.5.0',
+      ),
+    ),
+  ),
+);
